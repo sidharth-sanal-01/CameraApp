3f54dbbc78c897efb9d7ec92d22f26c400046403
sidharth-sanal-01 <sidharthsanal02@gmail.com>
2025-05-05 23:49:30 +0530

    Camera App 1.1
    Added Some Server functionalitites and other functionality to capture image
    and video
    Added Some Client functionality to read images and Video

---
+++ client/build/CMakeFiles/client.dir/src/CameraClient.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/client.dir/src/CameraClient.cpp.o: \
+ /home/sidharth/Desktop/Projects/CameraApp/client/src/CameraClient.cpp \
+ /usr/include/stdc-predef.h
---
+++ client/build/CMakeFiles/client.dir/src/ReadImage.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/client.dir/src/ReadImage.cpp.o: \
+ /home/sidharth/Desktop/Projects/CameraApp/client/src/ReadImage.cpp \
+ /usr/include/stdc-predef.h
